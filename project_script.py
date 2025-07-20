55061a6c712e6b7182e03919941f676307f8dfe3
Sarthak Choudhary <sarth8d314@gmail.com>
2025-07-20 16:47:40 +0200

    Clean up 🧹.

--- project_script.py
+++ project_script.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 file_name = 'source.csv'
-work_file = pd.read_csv(file_name)
+df = pd.read_csv(file_name)
 
-df = work_file.sort_values(by=['Datetime', 'Name']) # sort according to time.
 df['Total'] = 0 # add new column
+
+time_series = pd.to_datetime(df['Datetime']).dt.tz_localize(tz='UTC') # initiliase timezone in UTC
+time_series = time_series.dt.tz_convert(tz='Asia/Dhaka') # convert to UTC+6 timezone; Dhaka is in this timezone
+df['Datetime'] = time_series.dt.strftime('%Y-%m-%dT%H:%M:%S.%f') # convert datetime64 object to string
+del time_series
+
+df = df.sort_values(by=['Datetime', 'Name']) # sort according to time.
 for i, index_label in enumerate(df.index.tolist()):
     if df.loc[index_label]['Name'] == 'ProductA':
         if df.loc[index_label]['Purity'] == 'Pure':
@@ -19,11 +25,9 @@ for i, index_label in enumerate(df.index.tolist()):
             ProductB_correspond_ProductA_price = 0.75*df.iloc[i-1]['Price'] # use integer index not index label
         df.at[index_label, 'Total'] = df.loc[index_label]['Amount']*ProductB_correspond_ProductA_price
 
-time_s = pd.to_datetime(df['Datetime']).dt.tz_localize(tz='UTC') # tells pandas that the time is in UTC
-time_s = time_s.dt.tz_convert(tz='Asia/Dhaka') # converts to UTC+6 timezone; Dhaka is in this timezone
-df['Datetime'] = time_s.dt.strftime('%Y-%m-%dT%H:%M:%S.%f') # convert datetime64 object to string
-df = df.sort_index()
+df = df.sort_index() # sort a/c to index
 df['Datetime'] = df['Datetime'].str.slice(0, -3) # to truncate unneccesary zeros
 df['Total'] = df['Total'].round(2) # rounding to 2 decimal places
 
-df.to_csv('source_modified.csv', index=False) #export
+
+df.to_csv('source_modified.csv', index=False) # export to csv
