e5d386778a01276b9424a2fe91a22ac331c4c543
Sarthak Choudhary <sarth8d314@gmail.com>
2025-07-20 16:38:20 +0200

    Rounding Total to 2 decimal places.

--- project_script.py
+++ project_script.py
@@ -3,11 +3,8 @@ import pandas as pd
 file_name = 'source.csv'
 work_file = pd.read_csv(file_name)
 
-# n, m = work_file.shape
-
-df = work_file.sort_values(by=['Datetime', 'Name'])
-df['Total'] = 0
-
+df = work_file.sort_values(by=['Datetime', 'Name']) # sort according to time.
+df['Total'] = 0 # add new column
 for i, index_label in enumerate(df.index.tolist()):
     if df.loc[index_label]['Name'] == 'ProductA':
         if df.loc[index_label]['Purity'] == 'Pure':
@@ -23,10 +20,10 @@ for i, index_label in enumerate(df.index.tolist()):
         df.at[index_label, 'Total'] = df.loc[index_label]['Amount']*ProductB_correspond_ProductA_price
 
 time_s = pd.to_datetime(df['Datetime']).dt.tz_localize(tz='UTC') # tells pandas that the time is in UTC
-time_s = time_s.dt.tz_convert(tz='Asia/Dhaka') # Dhaka is in UTC+6 timezone
+time_s = time_s.dt.tz_convert(tz='Asia/Dhaka') # converts to UTC+6 timezone; Dhaka is in this timezone
 df['Datetime'] = time_s.dt.strftime('%Y-%m-%dT%H:%M:%S.%f') # convert datetime64 object to string
-
-df = df.sort_index() 
+df = df.sort_index()
 df['Datetime'] = df['Datetime'].str.slice(0, -3) # to truncate unneccesary zeros
+df['Total'] = df['Total'].round(2) # rounding to 2 decimal places
 
 df.to_csv('source_modified.csv', index=False) #export
